9367815473047d09f1aeb7cd839f5410cb50c3db
vladi93dev <vladi.sem93@gmail.com>
2026-03-11 00:47:46 +0200

    added GET, PUT, DELETE entry routes with auth

--- src/controllers/albumController.js
+++ src/controllers/albumController.js
@@ -1,6 +1,39 @@
 import { prisma } from '../config/db.js';
 import { generateToken } from '../config/generateToken.js';
 
+const getEntries = async (req, res) => {
+    try {
+        const entries = await prisma.entry.findMany({
+            where: {
+                userId: req.user.userId
+            }
+        });
+        res.status(200).json({ entries })
+    } catch(error) {
+        res.status(401).json({ message: error })
+    }
+}
+
+const getEntryById = async (req, res) => {
+    try {
+        const entry = await prisma.entry.findUnique({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if(!entry || entry.userId !== req.user.userId) {
+            return res.status(404).json({ message: "Entry not found" });
+        }
+
+        res.status(200).json(entry);
+
+        
+    } catch(error) {
+        res.status(400).json()
+    }
+}
+
 const createEntry = async (req, res) => {
     const { title, artist, genres, rating, note, userId } = req.body;
 
@@ -13,13 +46,57 @@ const createEntry = async (req, res) => {
             note,
             userId: req.user.userId
         }});
-        
-        
-
         res.status(200).json({ message: `${newEntry.artist} added` });
     } catch(error) {
         res.status(400).json({ message: `error: ${error.message}`});
     }
 }
 
-export { createEntry };
+const updateEntryById = async (req, res) => {
+    try {
+        const entry = await prisma.entry.update({
+            where: { id: req.params.id },
+            data: req.body 
+        });
+
+        if(!entry || entry.userId !== req.user.userId) {
+            return res.status(404).json({ error: "Entry not found"})
+        }
+
+        res.status(200).json(entry);
+        
+    } catch(error) {
+        return res.status(400).json({ message: `error: ${error}`})
+    }
+}
+
+const deleteEntryById = async(req, res) => {
+    try {
+        const entry = await prisma.entry.findUnique({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if(!entry || entry.userId !== req.user.userId) {
+            return res.status(400).json({ message: "Entry not found"});
+        }
+
+        await prisma.entry.delete({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        return res.status(200).json({message: `${entry.title} entry is deleted`});
+
+
+    } catch(error) {
+        return res.status(400).json(error);
+    }
+
+}
+
+
+
+export { createEntry, getEntries, getEntryById, updateEntryById, deleteEntryById };
--- src/routes/albums.route.js
+++ src/routes/albums.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createEntry } from '../controllers/albumController.js';
+import { createEntry, getEntries, getEntryById, updateEntryById, deleteEntryById } from '../controllers/albumController.js';
 import { generateToken } from '../config/generateToken.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import requestValidator from '../middlewares/requestValidator.js';
-import createEntrySchema from '../validators/createEntrySchema.js';
+import { createEntrySchema, updateEntrySchema } from '../validators/entrySchemas.js';
 
 
 const router = express.Router();
@@ -11,7 +11,15 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
+router.get('/', getEntries);
+
+router.get('/:id', getEntryById);
+
 router.post('/', requestValidator(createEntrySchema), createEntry);
+
+router.put('/:id', requestValidator(updateEntrySchema), updateEntryById);
+
+router.delete('/:id', deleteEntryById);
 
 
 
