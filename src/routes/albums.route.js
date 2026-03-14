3b22b94adbe6282ce4b35ced7a19eb6dea14c0a3
vladi93dev <vladi.sem93@gmail.com>
2026-03-15 01:57:44 +0200

    Add stats endpoint with (for now) total entries and average rating

--- src/routes/albums.route.js
+++ src/routes/albums.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createEntry, getEntries, getEntryById, updateEntryById, deleteEntryById, getEntriesGenres } from '../controllers/albumController.js';
+import { createEntry, getEntries, getEntryById, updateEntryById, deleteEntryById, getEntriesGenres, getStats } from '../controllers/albumController.js';
 import { generateToken } from '../config/generateToken.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import requestValidator from '../middlewares/requestValidator.js';
@@ -15,13 +15,17 @@ router.get('/', requestValidator(getEntryQuerySchema, 'query'), getEntries);
 
 router.get('/genres', getEntriesGenres);
 
+router.get('/stats', getStats);
+
 router.get('/:id', getEntryById);
+
 
 router.post('/', requestValidator(createEntrySchema), createEntry);
 
 router.put('/:id', requestValidator(updateEntrySchema), updateEntryById);
 
 router.delete('/:id', deleteEntryById);
+
 
 
 
